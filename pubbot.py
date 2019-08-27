4d51c15340656fe72545394c29e9811cb7f629fe
Lorenzo <lmacchiavelli@gmail.com>
2019-08-27 11:47:37 +0200

    Update pubbot.py

--- pubbot.py
+++ pubbot.py
@@ -1,27 +1,38 @@
-\#Importiamo le librerie
+#Importiamo le librerie
 import os
 from InstagramAPI import InstagramAPI
 import time
 import schedule
 
-\#Importiamo l'elenco di tutti i file presenti nella cartella coda_file in una lista
-fileNames = os.listdir("./coda_file/")
+#Importiamo l'elenco di tutti i file presenti nella cartella coda_file in una lista
+foldername = './coda_file/'
+fileNames = os.listdir(foldername)
 
-\#definiamo la funzione che pubblicherà i nostri file
+#definiamo la funzione che pubblicherà i nostri file
 def pubblica():
     print("..Pubblicazione")
 
-    IGUSER = "...Il tuo username di In"  
-    PASSWD = "...la tua password" 
+    IGUSER = '...Il tuo username di In'  
+    PASSWD = '...la tua password'
 
-    IGCaption = "Questa è la descrizione della nostra immagine posso aggiungere anche dei tag #motivazione #dailymotivational"
+    IGCaption = 'Questa è la descrizione della nostra immagine posso aggiungere anche dei tag #motivazione #dailymotivational...'
 
     igapi = InstagramAPI(IGUSER, PASSWD)
-    \#Colleghiamoci all'API di instgram
+    #Ci colleghiamo all'API di instgram
     igapi.login() 
-
-    \#eliminiamo la foto pubblicata
-    photo = './elaborati/'+ fileNames[0]
+    
+    #Pubblico l'immagine
+    photo = foldername'+ fileNames[0]
     igapi.uploadPhoto(photo, caption=IGCaption, upload_id=None)
-    print("Elimino : " + "./elaborati/" + fileNames[0])
-    os.remove("./elaborati/" + fileNames[0])
+    
+    #eliminiamo la foto pubblicata
+    print("Elimino : " + foldername + fileNames[0])
+    os.remove(foldername + fileNames[0])
+
+    
+#temporizziamo il tutto con il modulo schedule
+schedule.every().day.at("09:12").do(pubblica)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
